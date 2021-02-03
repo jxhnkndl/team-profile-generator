2fbed66f59763ed57be80058a76e897bd628b9a0
jxhnkndl <jkroyston@gmail.com>
2021-02-02 20:04:01 -0500

    Pass initial tests on Employee constructor

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -7,48 +7,48 @@ describe('Employee', () => {
   // Test object instantiation
   describe('Employee object instantiation', () => {
     it('Creates an object with name, id, and email properties when passed valid arguments', () => {
-      const newEmployee = new Employee('John Smith', 1, 'johnsmith@gmail.com');
+      const employee = new Employee("John Smith", 1, "johnsmith@gmail.com");
 
-      expect(new Employee()).toBeInstanceOf(Employee);
-      expect(newEmployee.name).toEqual('John Smith');
-      expect(newEmployee.id).toEqual(1);
-      expect(newEmployee.email).toEqual('johnsmith@gmail.com');
+      expect(employee).toBeInstanceOf(Employee);
+      expect(employee.name).toEqual('John Smith');
+      expect(employee.id).toEqual(1);
+      expect(employee.email).toEqual('johnsmith@gmail.com');
     });
   });
 
   // Test getName() method
   describe('getName', () => {
     it("Should return the object's name property", () => {
-      const newEmployee = new Employee('John Smith', 1, 'johnsmith@gmail.com');
+      const employee = new Employee('John Smith', 1, 'johnsmith@gmail.com');
 
-      expect(newEmployee.getName()).toEqual('John Smith');
+      expect(employee.getName()).toEqual('John Smith');
     });
   });
 
   // Test getId() method
   describe('getId', () => {
     it("Should return the object's id property", () => {
-      const newEmployee = new Employee('John Smith', 1, 'johnsmith@gmail.com');
+      const employee = new Employee('John Smith', 1, 'johnsmith@gmail.com');
 
-      expect(newEmployee.getId()).toEqual(1);
+      expect(employee.getId()).toEqual(1);
     });
   });
 
   // Test getEmail() method
   describe('getEmail', () => {
     it("Should return the object's email property", () => {
-      const newEmployee = new Employee('John Smith', 1, 'johnsmith@gmail.com');
+      const employee = new Employee('John Smith', 1, 'johnsmith@gmail.com');
 
-      expect(newEmployee.getEmail()).toEqual('johnsmith@gmail.com');
+      expect(employee.getEmail()).toEqual('johnsmith@gmail.com');
     });
   });
 
   // Test getRole() method
   describe('getRole', () => {
     it("Should return the string 'Employee'", () => {
-      const newEmployee = new Employee('John Smith', 1, 'johnsmith@gmail.com');
+      const employee = new Employee('John Smith', 1, 'johnsmith@gmail.com');
 
-      expect(newEmployee.getRole()).toEqual('Employee');
+      expect(employee.getRole()).toEqual('Employee');
     });
   });
 });
