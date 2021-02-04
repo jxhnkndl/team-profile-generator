a853039fac95534d890db11bd47e28a90677b054
jxhnkndl <jkroyston@gmail.com>
2021-02-04 17:37:06 -0500

    Refactor inquirer prompt sequences into separate manager, engineer, and intern arrays

--- index.js
+++ index.js
@@ -11,103 +11,106 @@ const Manager = require('./lib/Manager');
 // Team members
 const team = [];
 
-// Questions: Create manager
-const questions = [
-  [
-    {
-      name: 'init',
-      type: 'confirm',
-      message: 'Welcome to the Team Profile Generator. Are you ready to begin?',
-      
-    },
-    {
-      name: 'name',
-      type: 'input',
-      message: 'Your Name:',
-    },
-    {
-      name: 'id',
-      type: 'input',
-      message: 'Your Employee ID:'
-    },
-    {
-      name: 'email',
-      type: 'input',
-      message: 'Your Email Address:'
-    },
-    {
-      name: 'officeNumber',
-      type: 'input',
-      message: 'Your Office Number:'
-    },
-    {
-      name: 'upNext',
-      type: 'list',
-      choices: ['Add Engineer', 'Add Intern', 'Complete Team'],
-      message: 'What would you like to do for your team next?',
-    }
-  ],
-  [
-    {
-      name: 'name',
-      type: 'input',
-      message: "Engineer's Name:",
-    },
-    {
-      name: 'id',
-      type: 'input',
-      message: "Engineer's Employee ID:"
-    },
-    {
-      name: 'email',
-      type: 'input',
-      message: "Engineer's Email Address:"
-    },
-    {
-      name: 'github',
-      type: 'input',
-      message: "Engineer's GitHub Username:"
-    },
-    {
-      name: 'upNext',
-      type: 'list',
-      choices: ['Add Engineer', 'Add Intern', 'Complete Team'],
-      message: 'What would you like to do for your team next?',
-    }
-  ],
-  [
-    {
-      name: 'name',
-      type: 'input',
-      message: "Intern's Name:",
-    },
-    {
-      name: 'id',
-      type: 'input',
-      message: "Intern's Employee ID:"
-    },
-    {
-      name: 'email',
-      type: 'input',
-      message: "Intern's Email Address:"
-    },
-    {
-      name: 'school',
-      type: 'input',
-      message: "Intern's College or University:"
-    },
-    {
-      name: 'upNext',
-      type: 'list',
-      choices: ['Add Engineer', 'Add Intern', 'Complete Team'],
-      message: 'What would you like to do for your team next?',
-    }
-  ],
+// Prompts: Add new manager
+const newManager = [
+  {
+    name: 'role',
+    type: 'list',
+    choices: [
+      { value: 'manager', name: 'Start' },
+    ],
+    message: 'Welcome to the Team Profile Generator. Are you ready to begin?',
+  },
+  {
+    name: 'name',
+    type: 'input',
+    message: 'Your Name:',
+  },
+  {
+    name: 'id',
+    type: 'input',
+    message: 'Your Employee ID:',
+  },
+  {
+    name: 'email',
+    type: 'input',
+    message: 'Your Email Address:',
+  },
+  {
+    name: 'officeNumber',
+    type: 'input',
+    message: 'Your Office Number:',
+  },
+  {
+    name: 'upNext',
+    type: 'list',
+    choices: ['Add Engineer', 'Add Intern', 'Complete Team'],
+    message: 'What would you like to do for your team next?',
+  }    
 ];
 
-// Test questions (0 = Manager, 1 = Engineer, 2 = Intern)
+// Prompts: Add new engineer
+const newEngineer = [
+  {
+    name: 'name',
+    type: 'input',
+    message: "Engineer's Name:",
+  },
+  {
+    name: 'id',
+    type: 'input',
+    message: "Engineer's Employee ID:",
+  },
+  {
+    name: 'email',
+    type: 'input',
+    message: "Engineer's Email Address:",
+  },
+  {
+    name: 'github',
+    type: 'input',
+    message: "Engineer's GitHub Username:",
+  },
+  {
+    name: 'upNext',
+    type: 'list',
+    choices: ['Add Engineer', 'Add Intern', 'Complete Team'],
+    message: 'What would you like to do for your team next?',
+  }
+];
+
+// Prompts: Add new intern
+const newIntern = [
+  {
+    name: 'name',
+    type: 'input',
+    message: "Intern's Name:",
+  },
+  {
+    name: 'id',
+    type: 'input',
+    message: "Intern's Employee ID:",
+  },
+  {
+    name: 'email',
+    type: 'input',
+    message: "Intern's Email Address:",
+  },
+  {
+    name: 'school',
+    type: 'input',
+    message: "Intern's College or University:",
+  },
+  {
+    name: 'upNext',
+    type: 'list',
+    choices: ['Add Engineer', 'Add Intern', 'Complete Team'],
+    message: 'What would you like to do for your team next?',
+  },
+];
+
+// Control flow of inquirer prompts
 function ask(questionArr) {
-  // Start application by creating manager
   inquirer
     .prompt(questionArr)
     .then((member) => {
@@ -116,9 +119,9 @@ function ask(questionArr) {
 
       // Determine whether to create a new team member or exit app
       if (member.upNext === 'Add Engineer') {
-        ask(questions[1]);
+        ask(newEngineer);
       } else if (member.upNext === 'Add Intern') {
-        ask(questions[2]);
+        ask(newIntern);
       } else {
         console.log('Process complete.');
         console.log(team);
@@ -128,4 +131,4 @@ function ask(questionArr) {
 }
 
 // Init app
-ask(questions[0]);
+ask(newManager);
